f76992539ab14c2976a4b70882dfb6b75bbb9195
Tony900602 <tony900602@gmail.com>
2022-03-14 15:08:11 +0800

    update1

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+# save this as app.py
+import flask
+app = flask.Flask(__name__)
+
 @app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run()
